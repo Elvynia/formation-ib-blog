2246eab1842cf412bd9e2b8ce800a113ca4b8c16
unknown <elvynia@gmail.com>
2019-07-04 13:55:56 +0200

    Finlanisation refactoring avec Router.

--- src/app/form-view/form-view.component.ts
+++ src/app/form-view/form-view.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Article } from '../article';
 import { ArticleService } from '../article.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { filter, map, switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-form-view',
@@ -10,19 +12,36 @@ import { ArticleService } from '../article.service';
 export class FormViewComponent implements OnInit {
   editArticle: Article;
 
-  constructor(private service: ArticleService) {
+  constructor(private service: ArticleService,
+    private route: ActivatedRoute,
+    private router: Router) {
     this.editArticle = new Article();
   }
 
   ngOnInit() {
+    this.route.queryParamMap.pipe(
+      filter((paramMap) => paramMap.has('articleId')),
+      map((paramMap) => paramMap.get('articleId')),
+      map((id: string) => parseInt(id)),
+      tap((id) => console.log('Paramètre de route articleId=', id)),
+      switchMap((id: number) => this.service.read(id).pipe(
+        tap((article) => console.log('Article récupéré du backend : ', article))
+      ))
+    ).subscribe((article) => this.editArticle = article);
   }
 
   addArticle(article: any) {
     this.service.create(article.title, article.content);
+    this.backToHome();
   }
-
+  
   updateArticle(article: Article) {
     this.service.update(article);
+    this.backToHome();
+  }
+  
+  private backToHome() {
+    this.router.navigate(['home']);
   }
 
 }
--- src/app/list-view/list-view.component.ts
+++ src/app/list-view/list-view.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Article } from '../article';
 import { ListAction } from '../list/list.component';
 import { ArticleService } from '../article.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-view',
@@ -11,18 +12,26 @@ import { ArticleService } from '../article.service';
 export class ListViewComponent implements OnInit {
   articles: Array<Article>;
 
-  constructor(private service: ArticleService) {
+  constructor(private service: ArticleService,
+    private router: Router) {
     this.articles = new Array();
   }
 
   ngOnInit() {
+    this.service.readAll().subscribe(
+      (list) => this.articles = list
+    );
   }
 
   listAction(action: ListAction) {
     if (action.type === 'DELETE') {
       this.service.delete(action.payload);
     } else { // Edition !
-      // this.editArticle = JSON.parse(JSON.stringify(this.service.read(action.payload)));
+      this.router.navigate(['form'], {
+        queryParams: {
+          articleId: action.payload
+        }
+      });
     }
   }
 
